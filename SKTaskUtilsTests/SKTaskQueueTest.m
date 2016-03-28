e86de393877cbc00c844f8aa9b0c16ffb5b422e0
Shin-Kai Chen <skchen@sixnology.com>
2016-03-28 08:29:40 +0800

    Adjust test

--- SKTaskUtilsTests/SKTaskQueueTest.m
+++ SKTaskUtilsTests/SKTaskQueueTest.m
@@ -28,7 +28,7 @@
     [super tearDown];
 }
 
-- (void)test_shouldAddTask_whenTaskQueueIsEmpty {
+- (void)test_shouldAddTask {
     // given
     NSMutableArray *mockTaskArray = mock([NSMutableArray class]);
     [given([mockTaskArray count]) willReturnInt:0];
@@ -40,13 +40,6 @@
     
     // should
     [verify(mockTaskArray) addObject:mockTask];
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
