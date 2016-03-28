26eaef88e5b02631c08322ad7eb596b7039a60ec
Shin-Kai Chen <skchen@sixnology.com>
2016-03-28 09:07:11 +0800

    Add SKOrderedDictionary

---
+++ SKTaskUtils/SKOrderedDictionary.h
@@ -0,0 +1,13 @@
+//
+//  SKOrderedDictionary.h
+//  SKTaskUtils
+//
+//  Created by Shin-Kai Chen on 2016/3/28.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKOrderedDictionary : NSMutableDictionary
+
+@end
---
+++ SKTaskUtils/SKOrderedDictionary.m
@@ -0,0 +1,13 @@
+//
+//  SKOrderedDictionary.m
+//  SKTaskUtils
+//
+//  Created by Shin-Kai Chen on 2016/3/28.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import "SKOrderedDictionary.h"
+
+@implementation SKOrderedDictionary
+
+@end
